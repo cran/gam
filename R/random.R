79f5e7745dfae4afda08d769439a3829bde082fc
Trevor Hastie <hastie@stanford.edu>
2018-07-20 07:00:05 +0000

    version 1.16

--- R/random.R
+++ R/random.R
@@ -6,7 +6,7 @@
         stop("random() expects a factor or category as its first argument")
     newf=rep(0,length(f))
     attr(newf,"values")=f
-    attr(newf, "call") <- substitute(gam.random2(data[[scall]], z,
+    attr(newf, "call") <- substitute(gam.random(data[[scall]], z,
                                              w, df = df, lambda = lambda, intercept = intercept))
     oldClass(newf) <- "smooth"
     newf
